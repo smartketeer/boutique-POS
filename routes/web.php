0063a4268cd8d76d933f228c124686431864d89e
smartketeer <vincentsedimo03@gmail.com>
2026-05-22 22:10:28 +0800

    feat: add setup-storage route for shared hosting symlinks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/setup-storage', function () {
+    try {
+        Artisan::call('storage:link');
+        return 'Storage link created successfully!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
 
 Route::get('/{any}', function () {
     return view('welcome');
